2a2577d1e051b0735ae38b55882ffb63b27ea4a3
lukman-afex <labisoye@afexnigeria.com>
2022-08-25 09:47:07 +0100

    Initial commit

---
+++ forum/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Group(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class Reply(models.Model):
+    pass
