1cc78a5252bcf3ce24e9b2f2b2adac786f7b6416
Ahmad Sharafudeen <asharafudeen@afexnigeria.com>
2022-08-31 16:56:20 +0100

    made changes to url

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,11 +14,11 @@ from .views import (
 
 app_name = "accounts"
 urlpatterns = [
-    path("register", RegisterUserView.as_view(), name="register"),
-    path("login", UserLoginView.as_view(), name="login"),
-    path("logout", LogOutView.as_view(), name="logout"),
-    path("dashboard",  DashboardView.as_view(), name="dashboard"),
-    path("edit_profile", ProfileEditView.as_view(), name="edit_profile"),
+    path("register/", RegisterUserView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", LogOutView.as_view(), name="logout"),
+    path("dashboard/",  DashboardView.as_view(), name="dashboard"),
+    path("edit_profile/", ProfileEditView.as_view(), name="edit_profile"),
 
 
     path("activate/<uidb64>/<token>/", ActivateUserView.as_view(), name="activate"),
