fdb8e468fd3ab5b5254c8638b9b802811d9ca4b8
lukman-afex <labisoye@afexnigeria.com>
2022-08-31 15:25:07 +0100

    Views added

--- forum/urls.py
+++ forum/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'forum'
 
 urlpatterns = [
-    # path('', views.FeedView.as_view(), name='home'),
-    # path('groups', views.GroupListView.as_view(), name='groups'),
-    # path('group/<int:pk>', views.GroupDetailView.as_view(), name='group-detail'),
-    # path('group/<int:pk>/posts', views.PostListView.as_view(), name='group-posts'),
+    path('', views.FeedView.as_view(), name='home'),
+    path('groups', views.GroupListView.as_view(), name='groups'),
+    path('group/<int:pk>', views.GroupDetailView.as_view(), name='group-detail'),
+    path('group/<int:pk>/posts', views.PostListView.as_view(), name='group-posts'),
+    path('group/create', views.CreateGroupView.as_view(), name='group-create'),
     # path('group/<int:pk>/posts/<uuid:pk>', views.PostDetailView.as_view(), name='post-detail'),
 ]
