4f06c038babf226d956e0cba3e95089db5ef0701
Ahmad Sharafudeen <asharafudeen@afexnigeria.com>
2022-09-01 07:54:00 +0100

    pushed all changes

--- ShareIt/settings.py
+++ ShareIt/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'forum',
     'accounts',
+    'polls'
 ]
 
 MIDDLEWARE = [
--- ShareIt/urls.py
+++ ShareIt/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include('forum.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace="accounts")),
+    path('polls/', include('polls.urls', namespace='polls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
